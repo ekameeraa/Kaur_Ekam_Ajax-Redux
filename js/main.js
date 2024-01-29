533d03ac4d65e0b6393d61beb2a3639152066a7e
vasishth chaudhari <you@example.com>
2024-01-29 18:29:02 -0500

    updating

--- js/main.js
+++ js/main.js
@@ -1,70 +1,102 @@
 (() => {
-  const characterBox = document.querySelector("#movie-box");
+  const movieBox = document.querySelector("#movie-box");
   const reviewTemplate = document.querySelector("#review-template");
   const reviewCon = document.querySelector("#review-con");
   const baseUrl = 'https://swapi.dev/api/';
+  const imagesFolder = 'images/';
 
-  function fetchStarWarsCharacters() {
+  function getRandomFilmUrl(films) {
+    if (films && films.length > 0) {
+      const randomIndex = Math.floor(Math.random() * films.length);
+      return films[randomIndex];
+    } else {
+      return `${baseUrl}films/1/?format=json`;
+    }
+  }
+
+  function downloadAndStoreImage(imageUrl, imageName) {
+    // Implement code to download and store the image
+    // Placeholder - you need to implement this part based on your requirements
+  }
+
+  function getStarWarsCharacters() {
     fetch(`${baseUrl}people/?format=json`)
-      .then((response) => response.json())
-      .then(function (data) {
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
         const characters = data.results;
         const ul = document.createElement("ul");
-        characters.forEach((character) => {
+
+        characters.forEach(character => {
           const li = document.createElement("li");
           const a = document.createElement("a");
           a.textContent = character.name;
           a.href = "#";
-          a.dataset.characterUrl = character.url; // Store character URL
+          a.dataset.characterUrl = character.url;
           li.appendChild(a);
           ul.appendChild(li);
         });
-        characterBox.appendChild(ul);
+
+        movieBox.appendChild(ul);
       })
-      .then(function () {
-        const links = document.querySelectorAll("#movie-box li a");
-        links.forEach((link) => {
-          link.addEventListener("click", fetchCharacterMovieDetails);
+      .then(() => {
+        movieBox.addEventListener("click", function (e) {
+          if (e.target.tagName === 'A') {
+            e.preventDefault();
+            getCharacterFirstMovieDetails(e.target.dataset.characterUrl);
+          }
         });
       })
-      .catch((error) => {
-        console.log(error);
+      .catch(error => {
+        console.error(error);
       });
   }
 
-  function fetchCharacterMovieDetails(e) {
-    e.preventDefault();
-    const characterUrl = e.currentTarget.dataset.characterUrl;
+  function getCharacterFirstMovieDetails(characterUrl) {
+    let characterData;
+
     fetch(characterUrl)
-      .then((response) => response.json())
-      .then(function (characterData) {
-        // Display character details in the #review-con section
-        reviewCon.innerHTML = '';
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        characterData = data;
+        const randomFilmUrl = getRandomFilmUrl(characterData.films);
+        return fetch(randomFilmUrl);
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(filmData => {
         const template = document.importNode(reviewTemplate.content, true);
         const reviewHeading = template.querySelector(".review-heading");
         const reviewDescription = template.querySelector(".review-description");
 
-        // Get the movies the character was in
-        const filmUrls = characterData.films;
-        const filmPromises = filmUrls.map((filmUrl) =>
-          fetch(filmUrl)
-            .then((response) => response.json())
-            .then(function (filmData) {
-              // Append movie title and opening crawl to the reviewDescription
-              reviewDescription.textContent += `\n\nMovie: ${filmData.title}\nOpening Crawl: ${filmData.opening_crawl}`;
-            })
-        );
+        reviewHeading.textContent = characterData.name;
+        reviewDescription.innerHTML = `
+          <h4>${filmData.title}</h4>
+          <p>${filmData.opening_crawl}</p>`;
 
-        // Wait for all movie details to be fetched before displaying
-        Promise.all(filmPromises).then(() => {
-          reviewHeading.textContent = characterData.name;
-          reviewCon.appendChild(template);
-        });
+        const imageName = `movie_poster_${filmData.episode_id}.jpg`;
+        downloadAndStoreImage(`${baseUrl}images/${imageName}`, imageName);
+
+        reviewCon.innerHTML = '';
+        reviewCon.appendChild(template);
       })
-      .catch((error) => {
-        console.log(error);
+      .catch(error => {
+        console.error(error);
       });
   }
 
-  fetchStarWarsCharacters();
+  getStarWarsCharacters();
 })();

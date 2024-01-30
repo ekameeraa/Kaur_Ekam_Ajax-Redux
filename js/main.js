b90b5f59207019cd1e2832b66654f2e7fed34aa8
vasishth chaudhari <you@example.com>
2024-01-30 01:30:33 -0500

    updating

--- js/main.js
+++ js/main.js
@@ -5,42 +5,40 @@
   const baseUrl = 'https://swapi.dev/api/';
   const imagesFolder = 'images/';
   const spinner = `
-  <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="281px" height="281px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-    <g transform="translate(50 50) scale(0.73) translate(-50 -50)">
-      <g>
-        <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" calcMode="spline" dur="4s" values="0 50 50;90 50 50;180 50 50;270 50 50;360 50 50" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animateTransform>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" width="281px" height="281px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+      <g transform="translate(50 50) scale(0.73) translate(-50 -50)">
         <g>
-          <animateTransform attributeName="transform" type="scale" dur="1s" repeatCount="indefinite" calcMode="spline" values="1;1;0.5" keyTimes="0;0.5;1" keySplines="1 0 0 1;1 0 0 1"></animateTransform>
-          <g transform="translate(25 25)">
-            <rect x="-25" y="-25" width="52" height="52" fill="#e15b64">
-              <animate attributeName="fill" dur="4s" repeatCount="indefinite" calcMode="spline" values="#e15b64;#f47e60;#f8b26a;#abbd81;#e15b64" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animate>
-            </rect>
-          </g>
-          <g transform="translate(25 75)">
-            <rect x="-25" y="-25" width="52" height="50" fill="#e15b64">
-              <animateTransform attributeName="transform" type="scale" dur="1s" repeatCount="indefinite" calcMode="spline" values="0;1;1" keyTimes="0;0.5;1" keySplines="1 0 0 1;1 0 0 1"></animateTransform>
-              <animate attributeName="fill" dur="4s" repeatCount="indefinite" calcMode="spline" values="#e15b64;#f47e60;#f8b26a;#abbd81;#e15b64" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animate>
-            </rect>
-          </g>
-          <g transform="translate(75 25)">
-            <rect x="-25" y="-25" width="50" height="52" fill="#e15b64">
-              <animateTransform attributeName="transform" type="scale" dur="1s" repeatCount="indefinite" calcMode="spline" values="0;1;1" keyTimes="0;0.5;1" keySplines="1 0 0 1;1 0 0 1"></animateTransform>
-              <animate attributeName="fill" dur="4s" repeatCount="indefinite" calcMode="spline" values="#e15b64;#f47e60;#f8b26a;#abbd81;#e15b64" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animate>
-            </rect>
-          </g>
-          <g transform="translate(75 75)">
-            <rect x="-25" y="-25" width="50" height="50" fill="#e15b64">
-              <animateTransform attributeName="transform" type="scale" dur="1s" repeatCount="indefinite" calcMode="spline" values="0;1;1" keyTimes="0;0.5;1" keySplines="1 0 0 1;1 0 0 1"></animateTransform>
-              <animate attributeName="fill" dur="4s" repeatCount="indefinite" calcMode="spline" values="#e15b64;#f47e60;#f8b26a;#abbd81;#e15b64" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animate>
-            </rect>
+          <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" calcMode="spline" dur="4s" values="0 50 50;90 50 50;180 50 50;270 50 50;360 50 50" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animateTransform>
+          <g>
+            <animateTransform attributeName="transform" type="scale" dur="1s" repeatCount="indefinite" calcMode="spline" values="1;1;0.5" keyTimes="0;0.5;1" keySplines="1 0 0 1;1 0 0 1"></animateTransform>
+            <g transform="translate(25 25)">
+              <rect x="-25" y="-25" width="52" height="52" fill="#e15b64">
+                <animate attributeName="fill" dur="4s" repeatCount="indefinite" calcMode="spline" values="#e15b64;#f47e60;#f8b26a;#abbd81;#e15b64" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animate>
+              </rect>
+            </g>
+            <g transform="translate(25 75)">
+              <rect x="-25" y="-25" width="52" height="50" fill="#e15b64">
+                <animateTransform attributeName="transform" type="scale" dur="1s" repeatCount="indefinite" calcMode="spline" values="0;1;1" keyTimes="0;0.5;1" keySplines="1 0 0 1;1 0 0 1"></animateTransform>
+                <animate attributeName="fill" dur="4s" repeatCount="indefinite" calcMode="spline" values="#e15b64;#f47e60;#f8b26a;#abbd81;#e15b64" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animate>
+              </rect>
+            </g>
+            <g transform="translate(75 25)">
+              <rect x="-25" y="-25" width="50" height="52" fill="#e15b64">
+                <animateTransform attributeName="transform" type="scale" dur="1s" repeatCount="indefinite" calcMode="spline" values="0;1;1" keyTimes="0;0.5;1" keySplines="1 0 0 1;1 0 0 1"></animateTransform>
+                <animate attributeName="fill" dur="4s" repeatCount="indefinite" calcMode="spline" values="#e15b64;#f47e60;#f8b26a;#abbd81;#e15b64" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animate>
+              </rect>
+            </g>
+            <g transform="translate(75 75)">
+              <rect x="-25" y="-25" width="50" height="50" fill="#e15b64">
+                <animateTransform attributeName="transform" type="scale" dur="1s" repeatCount="indefinite" calcMode="spline" values="0;1;1" keyTimes="0;0.5;1" keySplines="1 0 0 1;1 0 0 1"></animateTransform>
+                <animate attributeName="fill" dur="4s" repeatCount="indefinite" calcMode="spline" values="#e15b64;#f47e60;#f8b26a;#abbd81;#e15b64" keyTimes="0;0.25;0.5;0.75;1" keySplines="0 1 0 1;0 1 0 1;0 1 0 1;0 1 0 1"></animate>
+              </rect>
+            </g>
           </g>
         </g>
       </g>
-    </g>
-  </svg>
+    </svg>
   `;
-
-
 
   function getRandomFilmUrl(films) {
     if (films && films.length > 0) {
@@ -139,7 +137,7 @@
         const template = document.importNode(reviewTemplate.content, true);
         const reviewHeading = template.querySelector(".review-heading");
         const reviewDescription = template.querySelector(".review-description");
-        const moviePoster = template.querySelector(".movie-poster");
+        const moviePosterContainer = template.querySelector(".movie-poster-container");
 
         reviewHeading.textContent = characterData.name;
         reviewDescription.innerHTML = `
@@ -149,7 +147,13 @@
         const imageName = `image${filmData.episode_id}.jpg`;
         downloadAndStoreImage(`${imagesFolder}${imageName}`, imageName);
 
-        moviePoster.src = `${imagesFolder}${imageName}`;
+        // Check if moviePosterContainer is not null before setting its content
+        if (moviePosterContainer) {
+          const img = document.createElement("img");
+          img.src = `${imagesFolder}${imageName}`;
+          img.alt = "Movie Poster";
+          moviePosterContainer.appendChild(img);
+        }
 
         reviewCon.innerHTML = '';
         reviewCon.appendChild(template);
